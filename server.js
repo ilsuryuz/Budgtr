3c87187c2226c1ae8160c27c99ae5df890fa4666
Ilsur <yuzlikeyevi@gmail.com>
2022-09-01 00:12:49 -0500

    Structured from in new.ejs, configured post method

--- server.js
+++ server.js
@@ -4,20 +4,37 @@ const bodyParser = require("body-parser")
 const app = express();
 const port = 3000;
 
-app.listen(port, () =>{
-    console.log("Chicago is listening")
-});
+
 // MIDDLEWARE
 app.use(express.static("Public"))
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.get("/budgets", (req, res) => {
     res.render("index.ejs", {
         data: budget,
     })
 })
 
-app.get("/budgets/:index", (req, res)=> {
-    res.render("show.ejs", {
-        dataIndex: budget[req.params.index]
+// NEW
+app.get("budgets/new", (req, res) => {
+    res.render("new.ejs", {
+        newData: budget,
     })
 })
+
+// CREATE
+app.post("/budgets", (req, res) => {
+    budget.push(req.body)
+    console.log(budget)
+    res.redirect("/budgets")
+})
+
+// SHOW
+app.get("/budgets/:index", (req, res) => {
+    res.render("show.ejs", {
+        dataIndex: budget[req.params.index],
+    })
+})
+
+app.listen(port, () => {
+    console.log("Chicago is listening")
+});
